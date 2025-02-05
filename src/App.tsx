2e83a5883a1d4e17e2a5c661511df1996b7c0f04
Colpno <gvinhh@gmail.com>
2025-02-05 14:03:36 +0700

    extract libraries-provider components into Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 
 import { Toast } from './components/index.ts';
-import './configs/dayjsConf.ts';
-import queryClient from './configs/reactQueryConf.ts';
+import Providers from './Providers.tsx';
 import router from './routes/index.ts';
 
 function App() {
   return (
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <Toast />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </HelmetProvider>
+    <Providers>
+      <RouterProvider router={router} />
+      <Toast />
+    </Providers>
   );
 }
 
