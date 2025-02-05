04e6ad386d6689aa60e716b230ed51a30cec904b
Colpno <gvinhh@gmail.com>
2025-02-05 12:15:34 +0700

    initialize react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 
 import './configs/momentConf.ts';
+import queryClient from './configs/reactQueryConf.ts';
 import router from './routes/index.ts';
 
 function App() {
   return (
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   );
 }
--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,3 +1,13 @@
+import { Method } from 'axios';
+
 export const STORAGE_KEYS = {
   ACCESS_TOKEN: 'accessToken',
+};
+
+export type RequestMethod = Extract<Method, 'GET' | 'POST' | 'PUT' | 'DELETE'>;
+export const REQUEST_METHODS: Record<RequestMethod, RequestMethod> = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
 };
