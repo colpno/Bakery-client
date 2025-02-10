f561d65dd71110464c5ba6f5fae4e6625bb5d7ef
Colpno <gvinhh@gmail.com>
2025-02-11 01:08:59 +0700

    add ThemeSetter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 
-import { Toast } from './components/index.ts';
 import Providers from './Providers.tsx';
 import router from './routes/index.ts';
+import ThemeSetter from './ThemeSetter.tsx';
+import Toast from './Toast.tsx';
 
 function App() {
   return (
     <Providers>
       <RouterProvider router={router} />
       <Toast />
+      <ThemeSetter />
     </Providers>
   );
 }
