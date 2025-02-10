9b12b019403d7043e1c70acd71d56d644f707b4f
Colpno <gvinhh@gmail.com>
2025-02-11 01:05:24 +0700

    override z-index class

--- src/utils/cssUtils.ts
+++ src/utils/cssUtils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...classNames: ClassValue[]) => {
   return twMerge(clsx(...classNames));
 };
+
+type ZIndexLayerName = string;
+type ZIndexMap = Record<ZIndexLayerName, number>;
+
+const createZIndexes = (layers: ZIndexLayerName[]): ZIndexMap => {
+  const INDEX_GAP = 10;
+
+  return layers.reduce((acc, layerName, index) => {
+    acc[layerName] = ++index * INDEX_GAP;
+    return acc;
+  }, {} as ZIndexMap);
+};
+
+/** First is smallest, last is largest. */
+const Z_INDEX_LAYERS: ZIndexLayerName[] = ['backdrop', 'popover', 'loading'];
+
+export const zIndexes = createZIndexes(Z_INDEX_LAYERS);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+import { zIndexes } from './src/utils/cssUtils.ts';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    zIndex: zIndexes,
+  },
+  plugins: [],
+};
