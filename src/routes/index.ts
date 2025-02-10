bca9039bdd7dfeb2abc5cc6e5fa141402023e740
Colpno <gvinhh@gmail.com>
2025-02-11 01:08:34 +0700

    implement Suspense

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,17 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { ROUTE_HOME } from '~/constants/routeConstants.ts';
 import ErrorBoundary from './ErrorBoundary.tsx';
+import Suspense from './Suspense.tsx';
 
 const router = createBrowserRouter([
   {
+    Component: Suspense,
     ErrorBoundary,
-    path: ROUTE_HOME,
+    children: [
+      {
+        path: ROUTE_HOME,
+      },
+    ],
   },
 ]);
 
