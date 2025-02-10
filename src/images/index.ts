9d46ff495c12f3ca895ec412e6eb59305d713213
Colpno <gvinhh@gmail.com>
2025-02-11 01:09:47 +0700

    add images

---
+++ src/images/index.ts
@@ -0,0 +1,4 @@
+export { default as noImageTextSVG } from './no-image-text.svg';
+export { default as noDataSVG } from './no-data.svg';
+export { default as noConnectionSVG } from './no-connection.svg';
+export { default as noSearchSVG } from './no-search.svg';
